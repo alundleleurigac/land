c181f47eb5aa33e1966e7266f94d3e39bbf9da7c
Chase Moskal <chasemoskal@gmail.com>
2025-09-28 01:19:52 -0700

    update: deps, view prep, install shiny

--- s/editor/context/context.ts
+++ s/editor/context/context.ts
@@ -1,15 +1,15 @@
 
-import {DropFirst, ob} from "@e280/stz"
-import {spa, View, ViewProps} from "@e280/sly"
+import {shiny, themes} from "@e280/shiny"
 import {Cellar, OpfsForklift} from "@e280/quay"
+import {contextualizeViews, spa} from "@e280/sly"
 
 import {Strata} from "./strata.js"
 import {Lol} from "../dom/views/lol/view.js"
 import {Nav} from "../dom/views/nav/view.js"
 import {AboutPage} from "../pages/about/view.js"
-import {CargoController} from "./controllers/cargo.js"
 import {UnknownPage} from "../pages/unknown/view.js"
 import {AccountPage} from "../pages/account/view.js"
+import {CargoController} from "./controllers/cargo.js"
 import {ProjectsPage} from "../pages/projects/view.js"
 import {getOmniMedia} from "../dom/components/omni-media/element.js"
 
@@ -38,12 +38,10 @@ export class EditorContext {
 		notFound: async() => UnknownPage(),
 	})
 
-	views = (() => {
-		const views = {Nav, Lol}
-		return ob(views).map(v => v.transmute((...p: any[]) => [this, ...p] as any)) as {
-			[K in keyof typeof views]: View<DropFirst<ViewProps<(typeof views)[K]>>>
-		}
-	})()
+	views = {
+		...shiny({theme: themes.basic}).views,
+		...contextualizeViews(this, {Nav, Lol}),
+	}
 
 	getElements = () => ({
 		OmniMedia: getOmniMedia(this),
